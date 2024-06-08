6c70c26ed1e495a69be324ee50a0263ddd82ab25
akkshara <akkshara.ss2021cce@sece.ac.in>
2024-06-08 16:21:40 +0530

    commit all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import Favorites from './Components/Favorites';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MovieSearch from './Components/MovieSearch';
+import SignupPage from './Components/SignupPage';
+import SigninPage from './Components/SigninPage';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/favorite" element={<Favorites />} />
+        <Route path="/" element={<SigninPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/home" element={<MovieSearch />} />
+      </Routes>
+    </Router>
+    );
 }
+
 
 export default App;
